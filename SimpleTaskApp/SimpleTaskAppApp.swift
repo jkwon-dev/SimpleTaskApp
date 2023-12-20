851d179643eedea3eb02f1f41ee9dfd4307e5bea
jkwon-dev <kwonejk@gmail.com>
2023-12-21 04:36:18 +0900

    Initial Commit

---
+++ SimpleTaskApp/SimpleTaskAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTaskAppApp.swift
+//  SimpleTaskApp
+//
+//  Created by kwon eunji on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTaskAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
